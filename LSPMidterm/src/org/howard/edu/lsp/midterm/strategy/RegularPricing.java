8dbf32668cbb777bf7c500dc19f36a8194578cce
Saleh2026 <167344669+Saleh2026@users.noreply.github.com>
2026-03-27 14:41:53 -0400

    Fully Completed Midterm
    Added RegularPricing class implementing PricingStrategy.

--- LSPMidterm/src/org/howard/edu/lsp/midterm/strategy/RegularPricing.java
+++ LSPMidterm/src/org/howard/edu/lsp/midterm/strategy/RegularPricing.java
@@ -1,3 +1,11 @@
 package org.howard.edu.lsp.midterm.strategy;
 
-public double computePrice(double price) { return price; }
+/**
+ * Strategy for regular customers with no discount.
+ */
+public class RegularPricing implements PricingStrategy {
+    @Override
+    public double computePrice(double price) {
+        return price;
+    }
+}
