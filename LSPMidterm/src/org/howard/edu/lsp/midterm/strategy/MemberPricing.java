cb65358d52b926b3fdaff54e9e43d1118a4649af
Saleh2026 <167344669+Saleh2026@users.noreply.github.com>
2026-03-27 14:41:10 -0400

    Fully Completed Midterm
    Refactor MemberPricing to implement PricingStrategy interface and add class documentation.

--- LSPMidterm/src/org/howard/edu/lsp/midterm/strategy/MemberPricing.java
+++ LSPMidterm/src/org/howard/edu/lsp/midterm/strategy/MemberPricing.java
@@ -1,3 +1,11 @@
 package org.howard.edu.lsp.midterm.strategy;
 
-public double computePrice(double price) { return price * 0.90; }
+/**
+ * Strategy for members with a 10% discount.
+ */
+public class MemberPricing implements PricingStrategy {
+    @Override
+    public double computePrice(double price) {
+        return price * 0.90;
+    }
+}
